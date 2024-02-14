803edc1d57f37f8d1dfd70329619fa128f7e51da
Marco Coscoy <marcocoscoy@s-164-67-211-121.resnet.ucla.edu>
2024-02-14 11:13:56 -0800

    Implemented basic navbar

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Search</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
