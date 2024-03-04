e8cb00911128913986689714f1a2fb69115ef59d
Nick Bailey <nbf150@me.com>
2024-03-04 13:14:38 -0800

    commit

--- src/albums.js
+++ src/albums.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { useState } from 'react';
+import './bubba-album.png';
+import './App.css';
+import { Link } from 'react-router-dom';
+
 const albumsBlurb = {
     fontWeight: 'bold',
     fontSize: '48px',
@@ -5,11 +11,20 @@ const albumsBlurb = {
     padding: '10px'
 }
 
-export default function Albums(){
+const albumButton = {
+
+}
+
+export default function AlbumsGrid(){
 
     return(
         <div style={albumsBlurb}>
-            Listened to a new album?
+            Listened to a new album? Drop your review.
         </div>
     )
+}
+
+function Album() {
+    return <button
+        className=albumButton={}
 }
