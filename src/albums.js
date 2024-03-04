3cbd341c21495737b9c76716eb3e09a3e3d000fa
Nick Bailey <nbf150@me.com>
2024-03-04 13:36:48 -0800

    albums grid implement1

--- src/albums.js
+++ src/albums.js
@@ -11,20 +11,47 @@ const albumsBlurb = {
     padding: '10px'
 }
 
-const albumButton = {
-
+const album1 = {
+    id: 'bubba',
+    url: '/public/bubba-album.png',
 }
 
-export default function AlbumsGrid(){
+const album2 = {
+    id: 'pond',
+    url: '/public/MIFLSA.png',
+}
 
-    return(
-        <div style={albumsBlurb}>
-            Listened to a new album? Drop your review.
+const albumsArray = [album1, album2];
+
+
+function AlbumGridComponent({ album, onClick}) {
+    return (
+        <div style={{ cursor: 'pointer' }} onClick={() => onClick(album.id)}>
+            <img src={album.url} style={{ width: '100%', height: 'auto', aspectRatio: '1 / 1' }} />
+        </div>
+    );
+}
+
+function AlbumGrid({albumsArray}) {
+    const handleClick = (id) => {
+        console.log('Image ${id} clicked!');
+        // handle click event later
+    }
+    return (
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))', gap: '10px'}}>
+            {albumsArray.map((album) => (
+                <AlbumGridComponent key={album.id} album={album} onClick={handleClick} />
+            ))}
         </div>
     )
 }
 
-function Album() {
-    return <button
-        className=albumButton={}
+export default function Top(){
+
+    return(
+        <div style={albumsBlurb}>
+            <h1>Listened to a new album? Drop your review.</h1>
+            <AlbumGrid albumsArray={albumsArray} />
+        </div>
+    )
 }
