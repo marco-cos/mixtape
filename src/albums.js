3d96bed7514b78158d906eeb29f9c11d179f1a68
Marco Coscoy <marcocoscoy@s-164-67-211-180.resnet.ucla.edu>
2024-03-04 14:18:06 -0800

    Made albums/browse reviews page
    Made frontend, with placeholder images and sections for popular albums, reviews from users you follow, and new reviews

--- src/albums.js
+++ src/albums.js
@@ -1,7 +1,69 @@
+import nextbutton from './rightarrow.png';
+
 export default function Albums(){
     return(
-        <div>
-            test albums yaya
+        
+        <div id="albumpage">
+            <h1>Popular Albums</h1>
+            <div id="albumcontainer">
+                <div className="albums_albumpage">
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://archive.smashing.media/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/aecf4604-1d3b-417f-97c6-d5be80f51eb9/3.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://hips.hearstapps.com/hmg-prod/images/7-64ecb1c909b78.png?crop=0.502xw:1.00xh;0.498xw,0&resize=1200:*"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://cdn.vox-cdn.com/thumbor/cUUdVnTXrleRNqbV-9JloWAleSI=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19535833/thumb_clean.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://imgv3.fotor.com/images/blog-richtext-image/born-to-die-music-album-cover.png"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://townsquare.media/site/366/files/2023/06/attachment-lil_uzi_vert_pink_tape_album_cover_artwork.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://pyxis.nymag.com/v1/imgs/699/1e2/965287137d49b3f29e6ff9c4d0e5a3f07b-01-kanye-west-ye.rsquare.w330.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://media.vanityfair.com/photos/561d1b04319af15240f9b03f/4:3/w_1280,h_960,c_limit/t-rihanna-cover-art-roy-nachum.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://static.stereogum.com/uploads/2023/03/LDR-Tunnel-1679672318-scaled.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://www.billboard.com/wp-content/uploads/2022/06/beyonce-Lemonade-album-art-billboard-1240.jpg?w=600"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://cdn.pastemagazine.com/www/articles/2019/11/25/strangerrrr.jpg"></img>
+                </div>
+                <div id="arrowdiv">
+                    <img id="arrow" src={nextbutton} width="100%" height="100%" />
+                </div>
+            </div>
+
+            <h1>Reviews From Users You Follow</h1>
+            <div id="albumcontainer">
+                <div className="albums_albumpage">
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://archive.smashing.media/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/aecf4604-1d3b-417f-97c6-d5be80f51eb9/3.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://hips.hearstapps.com/hmg-prod/images/7-64ecb1c909b78.png?crop=0.502xw:1.00xh;0.498xw,0&resize=1200:*"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://cdn.vox-cdn.com/thumbor/cUUdVnTXrleRNqbV-9JloWAleSI=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19535833/thumb_clean.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://imgv3.fotor.com/images/blog-richtext-image/born-to-die-music-album-cover.png"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://townsquare.media/site/366/files/2023/06/attachment-lil_uzi_vert_pink_tape_album_cover_artwork.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://pyxis.nymag.com/v1/imgs/699/1e2/965287137d49b3f29e6ff9c4d0e5a3f07b-01-kanye-west-ye.rsquare.w330.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://media.vanityfair.com/photos/561d1b04319af15240f9b03f/4:3/w_1280,h_960,c_limit/t-rihanna-cover-art-roy-nachum.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://static.stereogum.com/uploads/2023/03/LDR-Tunnel-1679672318-scaled.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://www.billboard.com/wp-content/uploads/2022/06/beyonce-Lemonade-album-art-billboard-1240.jpg?w=600"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://cdn.pastemagazine.com/www/articles/2019/11/25/strangerrrr.jpg"></img>
+                </div>
+                <div id="arrowdiv">
+                    <img id="arrow" src={nextbutton} width="100%" height="100%" />
+                </div>
+            </div>
+
+            <h1>New Reviews</h1>
+            <div id="albumcontainer">
+                <div className="albums_albumpage">
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://archive.smashing.media/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/aecf4604-1d3b-417f-97c6-d5be80f51eb9/3.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://hips.hearstapps.com/hmg-prod/images/7-64ecb1c909b78.png?crop=0.502xw:1.00xh;0.498xw,0&resize=1200:*"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://cdn.vox-cdn.com/thumbor/cUUdVnTXrleRNqbV-9JloWAleSI=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19535833/thumb_clean.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://imgv3.fotor.com/images/blog-richtext-image/born-to-die-music-album-cover.png"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://townsquare.media/site/366/files/2023/06/attachment-lil_uzi_vert_pink_tape_album_cover_artwork.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://pyxis.nymag.com/v1/imgs/699/1e2/965287137d49b3f29e6ff9c4d0e5a3f07b-01-kanye-west-ye.rsquare.w330.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://media.vanityfair.com/photos/561d1b04319af15240f9b03f/4:3/w_1280,h_960,c_limit/t-rihanna-cover-art-roy-nachum.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://static.stereogum.com/uploads/2023/03/LDR-Tunnel-1679672318-scaled.jpg"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://www.billboard.com/wp-content/uploads/2022/06/beyonce-Lemonade-album-art-billboard-1240.jpg?w=600"></img>
+                    <img className="cover_albumpage" width="100%" height="100%" src="https://cdn.pastemagazine.com/www/articles/2019/11/25/strangerrrr.jpg"></img>
+                </div>
+                <div id="arrowdiv">
+                    <img id="arrow" src={nextbutton} width="100%" height="100%" />
+                </div>
+            </div>
+
+
+
+        <br/>
         </div>
     )
 }
