0f708384f3180750fc2e20ded5a9b88dd673c2f7
Nick Bailey <nbf150@me.com>
2024-03-05 14:11:43 -0800

    made sections of albums page appear different by shuffling the 14 albums already there

--- src/albums.js
+++ src/albums.js
@@ -60,6 +60,8 @@ const album13 = {
 }
 
 const albums = [album0, album1, album2, album3, album4, album5, album6, album7, album8, album9, album10, album11, album12, album13];
+const albums_shuf0 = [album11,album3,album13,album9,album5,album4,album12,album2,album6,album10,album1,album7,album8,album0];
+const albums_shuf1 = [album8,album11,album0,album9,album5,album6,album10,album3,album2,album13,album7,album4,album12,album1];
 
 function AlbumGridComponent({ album, onClick}) {
     return (
@@ -97,11 +99,11 @@ export default function Albums() {
             </div>
             <h1>Reviewed By Your Friends</h1>
             <div>
-                <AlbumGrid albumsArray={albums} />
+                <AlbumGrid albumsArray={albums_shuf0} />
             </div>
             <h1>New Reviews</h1>
             <div>
-                <AlbumGrid albumsArray={albums} />
+                <AlbumGrid albumsArray={albums_shuf1} />
             </div>
         </div>
     )
