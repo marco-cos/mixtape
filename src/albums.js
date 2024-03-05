2c3a49e01efa6c85d2355a8718534b4e673fd1f6
Nick Bailey <nbf150@me.com>
2024-03-04 16:32:27 -0800

    updated keyframing

--- src/albums.js
+++ src/albums.js
@@ -50,7 +50,7 @@ function AlbumGridComponent({ album, onClick}) {
         <div style={{ cursor: 'pointer' }} onClick={() => onClick(album.id)}>
             <img 
                 src={album.url} 
-                className="fade-left"
+                className="pop-out"
                 width="100%"
                 height="100%" 
             />
