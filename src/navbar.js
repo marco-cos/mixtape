494b0de409cbbdaa160393976214a47a6a306414
Chloe Hera <chloekhera@yahoo.com>
2024-03-06 14:38:50 -0800

    add search bar

--- src/navbar.js
+++ src/navbar.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import logo from './logo.png';
+import searchicon from './searchicon.svg';
 import './App.css';
 import { Link } from 'react-router-dom'
 
 
 function Navbar() {
   return (
-    <div className="bar">
-      <div className="leftSection">
-        <div className="logo">
-        <li><Link to="/"><img src={logo} alt="logo"/></Link></li>
-        </div>
+    <nav>
+      <div className="logo">
+      <li><Link to="/"><img src={logo} alt="logo"/></Link></li>
       </div>
-      <div className="centerSpacing">
-      </div>
-      <div className="rightSection">
-        <div className="links">
-          <nav>
-            <ul id="navbartext">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/albums">Albums</Link></li>
-              <li><Link to="/profile">Profile</Link></li>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/register">Register</Link></li>
+      <div className="links">
+          <ul id="navbartext">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/albums">Albums</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
             </ul>
-          </nav>
-        </div>
+          <div className="search">
+              <input type="text" class="searchTerm" placeholder="search"></input>
+              <button type="submit">
+                <img src={searchicon} alt="go"/>
+              </button>
+          </div>
       </div>
-  </div>
+
+  </nav>
   );
 }
 
