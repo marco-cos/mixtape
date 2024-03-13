1d139f312402fa640fbc5296a16cdc5e796f9971
Marco Coscoy <marcocoscoy@s-164-67-211-115.resnet.ucla.edu>
2024-03-12 19:18:04 -0700

    Connected album page to backend, made controller, get reviews

--- server/controllers/albumpageController.js
+++ server/controllers/albumpageController.js
@@ -1,14 +1,29 @@
 const Albumpage = require("../models/albums");
+const Review = require("../models/review");
+const User = require("../models/user");
 
 exports.Albumpage = async (req, res, next) => {
   try {
     const { query } = req.body;
-    const albums = await Albumpage.findOne({"title": {$regex:query, $options: 'i'}});
+    const albums = await Albumpage.findById(query);
+    let reviewsArray = [];
 
-    res.json({albums});
+    for (let i = 0; i < albums.reviews.length; i++) {
+        const reviewID = albums.reviews[i].toString();
+        const reviewObj = (await Review.findById(reviewID)).toObject();
+        const reviewerID = reviewObj.reviewer.toString();
+        const reviewer = await User.findById(reviewerID);
+    
+        reviewObj.reviewer= reviewer.username;
+        reviewsArray.push(reviewObj);
+    }
+    res.json({albums: albums,
+            reviews: reviewsArray
+    });
   } 
   catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
--- server/models/albums.js
+++ server/models/albums.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose
 const albumSchema = new Schema({
     title: String,
     artist: String,
-    cover: Buffer,
+    cover: String,
     reviews: [{type: mongoose.Schema.Types.ObjectId, ref: 'Review'}],
     // follower: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     // user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
