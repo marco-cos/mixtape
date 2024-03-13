87ed26a89ab0746034334e29bf93f19121a65e28
Marco Coscoy <marcocoscoy@s-164-67-211-20.resnet.ucla.edu>
2024-03-13 14:47:08 -0700

    Added backend like functionality to album page

--- server/controllers/albumpageController.js
+++ server/controllers/albumpageController.js
@@ -2,7 +2,7 @@ const Albumpage = require("../models/albums");
 const Review = require("../models/review");
 const User = require("../models/user");
 
-exports.Albumpage = async (req, res, next) => {
+exports.getalbuminf = async (req, res, next) => {
   try {
     const { query } = req.body;
     const albums = await Albumpage.findOne({title: {$regex:query, $options: 'i'}});
@@ -13,12 +13,41 @@ exports.Albumpage = async (req, res, next) => {
         const reviewObj = (await Review.findById(reviewID)).toObject();
         const reviewerID = reviewObj.reviewer.toString();
         const reviewer = await User.findById(reviewerID);
+        reviewObj.ID=reviewID;
         reviewObj.reviewer= reviewer.username;
         reviewsArray.push(reviewObj); 
     }
     res.json({albums: albums,
             reviews: reviewsArray
     });
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.unlike = async (req, res) => {
+  console.log("IN UNLIKE")
+  try {
+    const { reviewID, userID } = req.body; 
+    console.log(reviewID,userID);
+    await Review.updateOne({ _id: reviewID },{ $pull: { likes: userID } });
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.like = async (req, res) => {
+  console.log("IN LIKE")
+  try {
+    const { reviewID, userID } = req.body; 
+    console.log(reviewID,userID);
+    await Review.updateOne({ _id: reviewID },{ $push: { likes: userID } });
+
+
   } 
   catch (error) {
     console.error(error);
