ec99792afd5eeaca9b2ec116084a8c8017c3bf22
Chloe Hera <chloekhera@yahoo.com>
2024-03-13 15:58:57 -0700

    update

--- src/components/postreview.js
+++ src/components/postreview.js
@@ -29,6 +29,7 @@ export default function Postreview(){
             // const albumId = albumResponse.data._id;
 
             const response = await axios.post('http://localhost:8000/createReview', {
+                albu
                 albumName: data.albumName,
                 rating: data.rating,
                 content: data.content,
